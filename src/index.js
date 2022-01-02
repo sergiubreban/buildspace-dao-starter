508538de46469466e58084522058229dcdf5126c
Sergiu Breban <contact@breban.ro>
2022-01-02 13:34:38 +0200

    add dapp

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+// import { ChakraProvider } from "@chakra-ui/react";
 
+// Include what chains you wanna support.
+// 4 = Rinkeby.
+const supportedChainIds = [4];
+
+// Include what type of wallet you want to support.
+// In this case, we support Metamask which is an "injected wallet".
+const connectors = {
+  injected: {},
+};
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    {/* <ChakraProvider> */ }
+    <ThirdwebWeb3Provider
+      connectors={ connectors }
+      supportedChainIds={ supportedChainIds }
+    >
+      <App />
+    </ThirdwebWeb3Provider>
+    {/* </ChakraProvider> */ }
+  </React.StrictMode >,
   document.getElementById("root")
 );
